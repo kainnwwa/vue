904041ccf1389bc1b4d4ebbc6436899561567f0a
Камила Кулушева <kamilakuluseva@gmail.com>
2026-05-03 21:36:55 +0500

    5

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -15,6 +15,7 @@
         <span :class="{ completed: задача.completed }">
           {{ задача.id }} - {{ задача.text }}
         </span>
+        <button @click="deleteTodo(задача)">Удалить</button>
       </li>
     </ul>
 
@@ -36,11 +37,49 @@ const todos = ref([
   { id: 1, text: 'Задача 1', completed: false },
   { id: 2, text: 'Задача 2', completed: false }
 ])
+
+function addTodo() {
+  if (newTodoText.value.trim() === '') {
+    alert('Введите текст задачи!')
+    return
+  }
+  
+  todos.value.push({
+    id: nextId++,
+    text: newTodoText.value,
+    completed: false
+  })
+  
+  newTodoText.value = ''
+}
+function deleteTodo(todo) {
+  todos.value = todos.value.filter(t => t.id !== todo.id)
+}
 </script>
 
 <style scoped>
 .completed {
   text-decoration: line-through;
   color: gray;
+}
+
+li {
+  margin: 10px 0;
+  list-style: none;
+}
+
+button {
+  margin-left: 10px;
+  background-color: #520707;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  padding: 2px 8px;
+  cursor: pointer;
+}
+
+input[type="text"] {
+  padding: 5px;
+  margin-right: 5px;
 }
 </style>
