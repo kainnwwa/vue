e84fd839b9ebedb5ec790dc077091f8fa1cbb9c2
Камила Кулушева <kamilakuluseva@gmail.com>
2026-05-03 21:19:29 +0500

    first

---
+++ todo-app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Счетчик: {{ nextId }}</p>
+    <input v-model="newTodoText" />
+    <div v-for="задача in todos" :key="задача.id">
+      {{ задача.id }} - {{ задача.text }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let nextId = 3
+const newTodoText = ref('')
+const todos = ref([
+  { id: 1, text: 'Задача 1', completed: false },
+  { id: 2, text: 'Задача 2', completed: false }
+])
+</script>
